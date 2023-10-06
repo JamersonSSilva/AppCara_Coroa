b5c50ab6bfce268ea3e587f69ae6f55a2951eeb6
Diego <admin@LAB3-24.bglab.local>
2023-10-05 21:25:03 -0300

    Aplicativo cara ou Coroa

---
+++ lib/jogar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class Jogar extends StatefulWidget {
+  String valor;
+   Jogar(this.valor);
+
+  @override
+  _JogarState createState() => _JogarState();
+}
+
+class _JogarState extends State<Jogar> {
+  @override
+  Widget build(BuildContext context) {
+    var caminhoImagem;
+    if(widget.valor == 'cara'){
+        caminhoImagem = "images/moeda_cara.png";
+    }
+    else{
+        caminhoImagem = "images/moeda_coroa.png";
+    }
+      
+    return Scaffold(
+      backgroundColor: Color(0xff61bd86),
+      body: Container(
+      
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset(caminhoImagem),
+            GestureDetector(
+              child: Image.asset("images/botao_voltar.png"),
+              onTap: (){
+                Navigator.pop(context);
+              },
+            )
+            ],
+        )
+      ),
+      
+    );
+  }
+}
